f0d386238671b037f509301c364023238921e489
Sunpoornarajan <zyntherra@gmail.com>
2026-04-05 21:58:00 +0530

    commit messages

---
+++ Leetcode/10.Contains Duplicate.java
@@ -0,0 +1,16 @@
+import java.util.HashMap;
+
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        HashMap<Integer, Boolean> map = new HashMap<>();
+
+        for (int i : nums) {
+            if (map.containsKey(i)) {
+                return true;
+            }
+            map.put(i, true);
+        }
+
+        return false;
+    }
+}
---
+++ Leetcode/12.Valid Palindrome.java
@@ -0,0 +1,36 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+
+        int start = 0;
+        int end = s.length() - 1;
+
+        while (start < end) {
+
+            if (Character.isLetter(s.charAt(start)) && Character.isLetter(s.charAt(end))) {
+
+                if (Character.toLowerCase(s.charAt(start)) != Character.toLowerCase(s.charAt(end)))
+                    return false;
+
+                start++;
+                end--;
+
+            } else if (Character.isDigit(s.charAt(start)) && Character.isDigit(s.charAt(end))) {
+
+                if (s.charAt(start) != s.charAt(end))
+                    return false;
+
+                start++;
+                end--;
+
+            } else if (!Character.isLetterOrDigit(s.charAt(start))) {
+                start++;
+            } else if (!Character.isLetterOrDigit(s.charAt(end))) {
+                end--;
+            } else {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
